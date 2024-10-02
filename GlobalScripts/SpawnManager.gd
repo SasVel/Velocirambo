d4a9a750bc52360fa1c7690e151217c7fb93e089
SasVel <sasvelchev@gmail.com>
2024-10-02 14:16:51 +0300

    spawn manager

---
+++ GlobalScripts/SpawnManager.gd
@@ -0,0 +1,26 @@
+extends Node
+
+##Manager for spawning objects.
+
+##Spawn size in meters.
+@export var spawnSize : float = 30
+@onready var healthPickupScn = preload("res://Components/Pickcups/health_pickup.tscn")
+
+var isSpawningPickups : bool = false :
+	set(val):
+		isSpawningPickups = val
+		if val: %PickupsTimer.start()
+		else: %PickupsTimer.stop()
+
+func get_random_spawn_position() -> Vector3:
+	return Vector3(randf_range(-spawnSize, spawnSize), 0, randf_range(-spawnSize, spawnSize))
+
+func spawn(objScn : PackedScene, pos = get_random_spawn_position()):
+	var inst = objScn.instantiate()
+	get_tree().get_root().add_child(inst)
+	inst.global_position = pos
+
+func _on_pickups_timer_timeout():
+	%PickupsTimer.wait_time = randf_range(20.0, 40.0)
+	spawn(healthPickupScn)
+	%PickupsTimer.start()
--- playScenes/playSceneScript.gd
+++ playScenes/playSceneScript.gd
@@ -3,6 +3,7 @@ extends Node3D
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	Music.play(Music.Tracks.Anky)
+	Spawn.isSpawningPickups = true
 
 func _input(event):
 	if event.is_action_pressed("Leftclick"):
