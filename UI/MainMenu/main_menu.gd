ad74e6cf18bee0a158d57d7781b61847d331dea2
SasVel <sasvelchev@gmail.com>
2024-10-25 13:03:17 +0300

    settings fixes

--- UI/MainMenu/main_menu.gd
+++ UI/MainMenu/main_menu.gd
@@ -3,11 +3,3 @@ extends Node3D
 func _ready():
 	Music.play(Music.Tracks.Anky)
 	get_tree().paused = false
-
-#func _input(event):
-	#if Input.get_vector("joy_camera_down", "joy_camera_up", "joy_camera_left", "joy_camera_right", 0.9) > Vector2.ZERO: 
-		#Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
-		#GameInfo.data.usingController = true
-	#elif event is InputEventMouseButton or InputEventMouseMotion:
-		#Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-		#GameInfo.data.usingController = false
