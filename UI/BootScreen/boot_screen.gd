dc91eb7f28bb3f34210e9aee492e4afbe2651fa2
SasVel <sasvelchev@gmail.com>
2025-05-05 19:25:27 +0300

    boot screen fixes

--- UI/BootScreen/boot_screen.gd
+++ UI/BootScreen/boot_screen.gd
@@ -4,15 +4,17 @@ extends Control
 @export var logoSprites : AnimatedSprite2D
 
 func _ready():
-    logoSprites.play("default")
-    var tween = create_tween()
-    tween.tween_property(logoSprites, "modulate:a", 1, 0.5).from(0)
-    await tween.finished
-
+	logoSprites.modulate.a = 0
+	logoSprites.frame = 0
+	await get_tree().create_timer(0.5).timeout
+	var tween = create_tween()
+	tween.tween_property(logoSprites, "modulate:a", 1, 0.5).from(0)
+	await tween.finished
+	logoSprites.play("default")
 
 func _on_logo_sprites_animation_finished():
-    await get_tree().create_timer(0.8).timeout
-    var tween = create_tween()
-    tween.tween_property(logoSprites, "modulate:a", 0, 1.5)
-    await tween.finished
-    get_tree().change_scene_to_packed(mainMenuScene)
+	await get_tree().create_timer(0.8).timeout
+	var tween = create_tween()
+	tween.tween_property(logoSprites, "modulate:a", 0, 1.5)
+	await tween.finished
+	get_tree().change_scene_to_packed(mainMenuScene)
